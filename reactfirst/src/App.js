28d8f26bab7066f05e9e6ba6a9f7531a15e14b69
Anna Hong <annahong312@gmail.com>
2022-10-10 23:15:56 -0700

    Add graph on button click

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -3,45 +3,81 @@ import React, { Component } from "react";
 import MultipleSelectGender from './Components/MultipleSelectGender.js';
 import MultipleSelectMajor from './Components/MultipleSelectMajor.js';
 import GenerateChart from './Components/GenerateChart.js';
-import AddGraphButton from './Components/AddGraphButton';
-
-import {useState} from "react"
-
-class App extends Component {
-
-render() {
-
-  var content = (
-    <div className="App">
-      {/* <header className="App-header"> </header> */}
-      <div className="background">
-        <button className="mainButton" type="button">Admin</button>
-        <h1>Center for Engineering Diversity Data Display Tool</h1>
+import { useState } from "react"
+import { color } from '@mui/system';
 
 
-        <div className="Checkbox-Background">
-          <p>Filter Options</p>
-          <MultipleSelectGender />
-          <MultipleSelectMajor />
-        </div>
-        <AddGraphButton />
+// Why did we use 'extends component' in the first place? I changed it to functional component so my hooks work
+function App() {
+
+
+  const [graphList, setGraphList] = useState([]);
+  const onAddBtnClick = event => {
+    setGraphList(graphList.concat(<GenerateChart />));
+  };
+
+
+  return (<div className="App">
+    {/* <header className="App-header"> </header> */}
+    <div className="background" >
+      <button className="mainButton" type="button">Admin</button>
+      <h1>Center for Engineering Diversity Data Display Tool</h1>
+
+
+      <div className="Checkbox-Background">
+        <p>Filter Options</p>
+        <MultipleSelectGender />
+        <MultipleSelectMajor />
       </div>
-
-
-      <div className="background">
-        <h1>Chart</h1>
-        <GenerateChart />
-      </div>
-      <div className="background">
-        <h1>Graph</h1>
-      </div>
+      <button onClick={onAddBtnClick} className="mainButton" type="button">Generate Data</button>
     </div>
+
+
+    <div style={{paddingBottom:'300px'}}>
+      <h1>Chart</h1>
+      {graphList}
+    </div>
+    
+    <div className="background">
+      <h1>Graph</h1>
+    </div>
+  </div>
   );
-  return content;
+}
+
+// class App extends Component {
+//   render() {
+//     var content = (
+//       <div className="App">
+//         {/* <header className="App-header"> </header> */}
+//         <div className="background">
+//           <button className="mainButton" type="button">Admin</button>
+//           <h1>Center for Engineering Diversity Data Display Tool</h1>
 
 
-}
-}
+//           <div className="Checkbox-Background">
+//             <p>Filter Options</p>
+//             <MultipleSelectGender />
+//             <MultipleSelectMajor />
+//           </div>
+//           <AddGraphButton />
+//         </div>
+
+
+//         <div className="background">
+//           <h1>Chart</h1>
+//           <GenerateChart />
+//         </div>
+//         <div className="background">
+//           <h1>Graph</h1>
+//         </div>
+//       </div>
+//     );
+//     return content;
+
+
+//   }
+// }
 
 
 export default App;
