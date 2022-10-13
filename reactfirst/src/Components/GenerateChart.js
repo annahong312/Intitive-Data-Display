c6cd08f567cc960769cf87c5b6b0ffc52cb1a94a
Eleanor Desai <eleanord@usc.edu>
2022-10-13 12:37:12 -0700

    I hate this chart thingy

--- reactfirst/src/Components/GenerateChart.js
+++ reactfirst/src/Components/GenerateChart.js
@@ -1,5 +1,5 @@
 // import React, { Component } from "react";
-/*import {Bar} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
 
 const chartState = {
   labels: ['January', 'February', 'March',
@@ -36,4 +36,4 @@ export default function GenerateChart() {
       </div>
     );
 
-}*/
+}
