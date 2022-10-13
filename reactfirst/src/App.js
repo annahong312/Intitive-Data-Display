42a20612e85e8fe35dd3d6be501f0fc0ba041298
Eleanor Desai <eleanord@usc.edu>
2022-10-13 12:33:00 -0700

    Hopefully no more tears

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -3,14 +3,15 @@ import React, { Component } from "react";
 import MultipleSelectGender from './Components/MultipleSelectGender.js';
 import MultipleSelectMajor from './Components/MultipleSelectMajor.js';
 import GenerateChart from './Components/GenerateChart.js';
+import Login from './Login.js';
 
 class App extends Component {
 
   render() {
-
     var content = (
       <div className="App">
         {/* <header className="App-header"> </header> */}
+        <Login/>
         <div className="background">
           <button className="mainButton" type="button">Admin</button>
           <h1>Center for Engineering Diversity Data Display Tool</h1>
@@ -26,7 +27,7 @@ class App extends Component {
 
         <div className="background">
           <h1>Chart</h1>
-          <GenerateChart/>
+          {/* <GenerateChart/> */}
         </div>
         <div className="background">
           <h1>Graph</h1>
