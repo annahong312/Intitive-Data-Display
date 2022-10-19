c61ec72ce35e222f3d2c9ce5cc76350880b2956e
Lydia DiBlasio <diblasio@usc.edu>
2022-10-18 23:54:52 -0700

    add tab visual

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -4,8 +4,9 @@ import GenerateChartMUI from './Components/GenerateChartMUI.js';
 import GenerateGraph from './Components/GenerateGraph.js';
 import MultipleSelectGender from './Components/MultipleSelectGender.js';
 import MultipleSelectMajor from './Components/MultipleSelectMajor.js';
-
-
+  
+var curIndex = -1;
+var maxIndex = -1;
 
 // Why did we use 'extends component' in the first place? I changed it to functional component so my hooks work
 function App() {
@@ -13,10 +14,15 @@ function App() {
   const [chartList, setChartList] = useState([]);
 
   const [graphList, setGraphList] = useState([]);
+  // build tab list
+  const [tabList, setTabList] = useState([]);
   const onAddBtnClickGraph = event => {
     setGraphList(graphList.concat(<GenerateGraph />));
     // setChartList(chartList.concat(<DataTable />));
     setChartList(chartList.concat(<GenerateChartMUI />));
+    curIndex++;
+    maxIndex++;
+    setTabList(tabList.concat(<button class="tablinks" onclick="">Tab {curIndex+1}</button>));
   };
 
 
@@ -35,54 +41,24 @@ function App() {
       <button onClick={onAddBtnClickGraph} className="mainButton" type="button">Generate Data</button>
     </div>
 
+    <div class="tab" style={{paddingBottom:'300px'}}>
+      {tabList}
+    </div>
 
     <div style={{paddingBottom:'300px'}}>
       <h1>Chart</h1>
       {/* <DataTable></DataTable> */}
-      {chartList}
+      {chartList[curIndex]}
     </div>
+
     
     <div className="background">
       <h1>Graph</h1>
-      {graphList}
+      {graphList[curIndex]}
     </div>
   </div>
   );
 }
-
-// class App extends Component {
-//   render() {
-//     var content = (
-//       <div className="App">
-//         {/* <header className="App-header"> </header> */}
-//         <div className="background">
-//           <button className="mainButton" type="button">Admin</button>
-//           <h1>Center for Engineering Diversity Data Display Tool</h1>
-
-
-//           <div className="Checkbox-Background">
-//             <p>Filter Options</p>
-//             <MultipleSelectGender />
-//             <MultipleSelectMajor />
-//           </div>
-//           <AddGraphButton />
-//         </div>
-
-
-//         <div className="background">
-//           <h1>Chart</h1>
-//           <GenerateChart />
-//         </div>
-//         <div className="background">
-//           <h1>Graph</h1>
-//         </div>
-//       </div>
-//     );
-//     return content;
-
-
-//   }
-// }
 
 
 export default App;
