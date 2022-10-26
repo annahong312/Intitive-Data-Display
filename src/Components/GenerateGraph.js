ccba10e3501a2394cb08bdc132499398579419f5
Lydia DiBlasio <diblasio@usc.edu>
2022-10-26 11:26:36 -0700

    fix graph to update on tab click

--- src/Components/GenerateGraph.js
+++ src/Components/GenerateGraph.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Line } from 'react-chartjs-2';
 // import { Chart } from 'chart.js/auto'
 import {
@@ -29,47 +29,80 @@ export default function GenerateGraph(index) {
   // Data For Graph
   const dataFromFile = parseInt(index.index) % 2 > 0 ? UserData2 : UserData;
   console.log(index.index + " is index");
-  console.log(dataFromFile);
+  // print data from file user gain values
 
-  const [data, setData] = useState({
-    labels: dataFromFile.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: dataFromFile.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-    options: {
-      plugins: {
-          title: {
-              display: true,
-              text: 'Custom Chart Title',
-              padding: {
-                  top: 10,
-                  bottom: 30
-              }
-          }
-      }
-  }
-  });
+  // console.log(dataFromFile[0][2] + " is dataFromFile");
+
+  // const [data, setData] = useState({
+  //   labels: dataFromFile.map((data) => data.year),
+  //   datasets: [
+  //     {
+  //       label: "Users Gained",
+  //       data: dataFromFile.map((data) => data.userGain),
+  //       backgroundColor: [
+  //         "rgba(75,192,192,1)",
+  //         "#ecf0f1",
+  //         "#50AF95",
+  //         "#f3ba2f",
+  //         "#2a71d0",
+  //       ],
+  //       borderColor: "black",
+  //       borderWidth: 2,
+  //     },
+  //   ],
+  //   options: {
+  //     plugins: {
+  //         title: {
+  //             display: true,
+  //             text: 'Custom Chart Title',
+  //             padding: {
+  //                 top: 10,
+  //                 bottom: 30
+  //             }
+  //         }
+  //     }
+  // }
+  // });
 
   // print usergain from data hook object
-  console.log(data.datasets[0].data + " is user gain");
+  // console.log(data.datasets[0].data + " is user gain");
+  console.log(dataFromFile.map((data) => data.userGain));
   // console.log(data.userGain + " is userData");
   return (
     <div>
-      <button onClick={() => setData(data)}>Reset</button>
+      {/* <button onClick={() => setData(data)}>Reset</button> */}
       <Line
-        data={data}
+        // data={data}
+        data={{
+          labels: dataFromFile.map((data) => data.year),
+          datasets: [
+            {
+              label: "Users Gained",
+              data: dataFromFile.map((data) => data.userGain),
+              backgroundColor: [
+                "rgba(75,192,192,1)",
+                "#ecf0f1",
+                "#50AF95",
+                "#f3ba2f",
+                "#2a71d0",
+              ],
+              borderColor: "black",
+              borderWidth: 2,
+            },
+          ],
+          options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Custom Chart Title',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                }
+            }
+        }
+      }}
         height={"80px"}
       // width={"30%"}
       // options={{ maintainAspectRatio: false }}
